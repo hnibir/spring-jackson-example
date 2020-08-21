b7b58e643b47144441718766e24d75e5ae6d1c5f
Nibir Hossain <nibir.hossain85@gmail.com>
2020-08-21 13:26:09 +0200

    Jackson property naming strategies: add kebab case strategy

---
+++ src/test/java/spring/micro/services/springjacksonexample/model/BeerDtoKebabTest.java
@@ -0,0 +1,26 @@
+package spring.micro.services.springjacksonexample.model;
+
+/*
+ * Created by Nibir Hossain on 21.08.20
+ */
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("kebab")
+@JsonTest
+public class BeerDtoKebabTest extends BaseTest {
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void testKebabCase() throws JsonProcessingException {
+        BeerDto beerDto = getBeerDto();
+        String json = objectMapper.writeValueAsString(beerDto);
+        System.out.println(json);
+    }
+}
